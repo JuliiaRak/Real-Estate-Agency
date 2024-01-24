7b67d4cd3650038ff1f63ab99d29495cce9a62cd
DenysKulikov <dkulikov.laba@solvd.com>
2024-01-24 10:39:54 +0200

    Removed unused exception

--- src/main/java/com/solvd/domain/exceptions/CreateConnectionException.java
+++
@@ -1,7 +0,0 @@
-package com.solvd.domain.exceptions;
-
-public class CreateConnectionException extends RuntimeException {
-    public CreateConnectionException(Throwable cause) {
-        super(cause);
-    }
-}

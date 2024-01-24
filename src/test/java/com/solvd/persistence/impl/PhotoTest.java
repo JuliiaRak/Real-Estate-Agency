2337f41b21b3082b63ffd2c5dfdb572c946d6a0c
Artem Kurkin <kurkin566@gmail.com>
2024-01-24 14:53:57 +0200

    Added Tags and Photos (#5)
    * Added Tags and Photos, created following domain classes, mappers and repositories
    
    * Fulfilled RealEstate entity with collections of photos and tags, updated mappers due to this changes
    
    * Added test cases for photo and tag repositories

---
+++ src/test/java/com/solvd/persistence/impl/PhotoTest.java
@@ -0,0 +1,29 @@
+package com.solvd.persistence.impl;
+
+import com.solvd.domain.Photo;
+import com.solvd.persistence.PhotoRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PhotoTest {
+    private static final Logger LOGGER = LogManager.getLogger(PhotoTest.class);
+
+    public static void main(String[] args) {
+        PhotoRepository repository = new PhotoRepositoryMybatisImpl();
+
+        Photo photo = new Photo();
+        photo.setLink("g.com/p1");
+        repository.create(photo, 1);
+        photo.setLink("g.com/p2");
+        repository.create(photo, 1);
+        photo.setLink("g.com/p3");
+        repository.create(photo, 2);
+        LOGGER.info("All: " + repository.findAll());
+        LOGGER.info("All by RE with id 1: " + repository.findAllByRealEstateId(1));
+        LOGGER.info("All by RE with id 2: " + repository.findAllByRealEstateId(2));
+        repository.deleteByRealEstateId(1);
+        LOGGER.info("Should be empty -> " + repository.findAllByRealEstateId(1));
+        LOGGER.info("Should be true -> " + repository.findAllByRealEstateId(1).isEmpty());
+        repository.deleteByRealEstateId(2);
+    }
+}

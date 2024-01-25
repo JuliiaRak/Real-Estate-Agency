84077952e77b12b9ebae351c307703160ee07d06
JuliiaRak <91924230+JuliiaRak@users.noreply.github.com>
2024-01-25 19:16:03 +0200

    Added validation to Employee and Meeting Service classes (#15)
    * Created basic dao, domain and basic service for Emploee and Meeting
    
    * Added validation to Employee and Meeting Service classes
    
    * Deleted Optional from Meeting getById
    
    ---------
    
    Co-authored-by: JuliiaRak <julia.rak.informatyka@gmail.com>
    Co-authored-by: Denys Kulikov <149414019+DenysKulikov@users.noreply.github.com>

--- src/main/java/com/solvd/service/MeetingService.java
+++ src/main/java/com/solvd/service/MeetingService.java
@@ -1,6 +1,7 @@
 package com.solvd.service;
 
 import com.solvd.domain.Meeting;
+import com.solvd.domain.exceptions.EntityNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,7 +9,7 @@ import java.util.Optional;
 public interface MeetingService {
     void create(Meeting meeting, Long realEstateId, Long buyerId, Long employeeId);
     void deleteById(long id);
-    void update(Meeting meeting, Long realEstateId, Long buyerId, Long employeeId);
-    Optional<Meeting> getById(long id);
+    void update(Meeting meeting, Long realEstateId, Long buyerId, Long employeeId) throws EntityNotFoundException;
+    Meeting getById(long id) throws EntityNotFoundException;
     List<Meeting> getAll();
 }

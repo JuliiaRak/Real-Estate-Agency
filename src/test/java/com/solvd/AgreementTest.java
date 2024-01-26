c67f75d59274f75b15a1f58bb361409262fc2ac4
jlmp3mp44 <114174334+jlmp3mp44@users.noreply.github.com>
2024-01-26 12:59:53 +0200

    Jmeleshko (#16)
    * Made model, repository, service for Agreement
    
    * Resolve problem with id Agreements
    
    * Find another problems with null, solvd them
    
    * Replace sql.Date with util.Date
    
    * Change double to BigDecimal, change Main
    
    * Made changes with assosiations realEstate and Client in the Agreements
    
    * Change sql select
    
    * Made validation for service Agreement
    
    * Made services validation
    
    * Add validatinon
    
    * Delete logs directory
    
    * Remove config.properties
    
    * Pull from main
    
    * Modify
    
    ---------
    
    Co-authored-by: Julya <julya.meleshko@gmail.com>
    Co-authored-by: Denys Kulikov <149414019+DenysKulikov@users.noreply.github.com>

--- src/test/java/com/solvd/AgreementTest.java
+++ src/test/java/com/solvd/AgreementTest.java
@@ -31,11 +31,12 @@ import java.util.List;
 
 
 public class AgreementTest {
-    public static void main(String[] args) throws EntityNotFoundException {
-
+    public static void main(String[] args) throws EntityNotFoundException, PhoneNumberAlreadyExistException, EmailAlreadyExistException {
         //creating instances
         Client client = new Client();
         Client seller = new Client();
+        client.setEmail("00aj20121890231129@gmail.com");
+        client.setPhoneNumber("0851000012");
         client.setEmail("aj2619@gmail.com");
         client.setPhoneNumber("008082");
         client.setFirstName("Anna");
@@ -44,6 +45,8 @@ public class AgreementTest {
 
         seller.setFirstName("Ivan");
         seller.setLastName("Kulikov");
+        seller.setEmail("ikd0004233124003v@gmail.com");
+        seller.setPhoneNumber("1202-1252342");
         seller.setEmail("ikd46778ev@gmail.com");
         seller.setPhoneNumber("+232-2542");
         seller.setRegistrationDate(new Date());
@@ -68,17 +71,28 @@ public class AgreementTest {
 
 
         Agreement agreement = new Agreement();
+        Agreement agreement2 =  new Agreement();
         agreement.setAmount(BigDecimal.valueOf(37218));
         agreement.setDate(new Date());
         agreement.setClient(client);
         agreement.setStatus("Paid");
         agreement.setRealEstate(realEstate);
+        agreement.setDuration("Indefinite");
+
+        agreement2.setAmount(BigDecimal.valueOf(500));
+        agreement2.setDate(new Date(2023-11-02));
+        agreement2.setClient(client);
+        agreement2.setStatus("unpaid");
+        agreement2.setRealEstate(realEstate);
+        agreement2.setDuration("3 months");
         agreement.setDuration("3 month");
 
         //creating repositories, services
 
         ClientRepository clientRepository = new ClientRepositoryMybatisImpl();
         ClientService clientService = new ClientServiceImpl(clientRepository);
+        clientService.create(client);
+        clientService.create(seller);
         try {
             clientService.create(client);
             clientService.create(seller);
@@ -90,11 +104,30 @@ public class AgreementTest {
         AddressRepository addressRepository = new AddressRepositoryMybatisImpl();
         AddressService addressService = new AddressServiceImpl(addressRepository);
         addressService.create(address);
+
         System.out.println(address);
 
         RealEstateRepository realEstateRepository = new RealEstateRepositoryMybatisImpl();
         RealEstateService realEstateService = new RealEstateServiceImpl(realEstateRepository, addressService);
         realEstateService.create(realEstate, client.getId());
+        //System.out.println(realEstate);
+
+        AgreementRepository agreementRepository = new AgreementRepositoryMyBatisImpl();
+        AgreementService agreementService = new AgreementServiceImpl(agreementRepository,realEstateService);
+
+        //check CRUD operations
+
+        /*agreement.setDuration("4 months");
+        agreementService.update(agreement);
+
+        System.out.println(agreementService.getById(5));
+
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> 84077952e77b12b9ebae351c307703160ee07d06
+=======
+>>>>>>> a85536f3d0ec39fc53a2b4c4fd6b2d65dcdfc5b2
         System.out.println(realEstate);
 
         AgreementRepository agreementRepository = new AgreementRepositoryMyBatisImpl();
@@ -107,16 +140,36 @@ public class AgreementTest {
         agreement.setDuration("4 months");
         agreementService.update(agreement);
 
-
-
-
         System.out.println(agreementService.getById(5));
 
+<<<<<<< HEAD
 
+<<<<<<< HEAD
+>>>>>>> 84077952e77b12b9ebae351c307703160ee07d06
+=======
+>>>>>>> 84077952e77b12b9ebae351c307703160ee07d06
+=======
+>>>>>>> a85536f3d0ec39fc53a2b4c4fd6b2d65dcdfc5b2
         agreementService.deleteById(1);
         agreementService.deleteById(2);
         agreementService.deleteById(3);
         agreementService.deleteById(4);
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> a85536f3d0ec39fc53a2b4c4fd6b2d65dcdfc5b2
+        List<Agreement> agreements = agreementService.getAll();
+        for (Agreement agrm : agreements) {
+            System.out.println(agrm);
+        }*/
+
+        //check the validation
+        agreementService.create(agreement2, realEstate.getId(), client.getId());
+        System.out.println(realEstateService.getById(realEstate.getId()));
+
+        System.out.println(agreement2);
 
         //List<Agreement> agreements = agreementService.getAll();
         //for (Agreement agrm : agreements) {

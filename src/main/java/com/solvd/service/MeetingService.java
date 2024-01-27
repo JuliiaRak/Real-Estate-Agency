517832fee874e69134495ca2bf5d704f50ff27ac
Artem Kurkin <kurkin566@gmail.com>
2024-01-27 11:39:56 +0200

    Service improvements(validation and logic) (#24)
    * completed validation for create methods
    
    * changed ON DELETE NO ACTION to CASCADE
    
    * updated photo validation
    
    * enhanced validation for unique email and phone, added missing checks

--- src/main/java/com/solvd/service/MeetingService.java
+++ src/main/java/com/solvd/service/MeetingService.java
@@ -4,12 +4,15 @@ import com.solvd.domain.Meeting;
 import com.solvd.domain.exceptions.EntityNotFoundException;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MeetingService {
-    void create(Meeting meeting, Long realEstateId, Long buyerId, Long employeeId);
+    void create(Meeting meeting, Long realEstateId, Long buyerId, Long employeeId) throws EntityNotFoundException;
+
     void deleteById(long id);
+
     void update(Meeting meeting, Long realEstateId, Long buyerId, Long employeeId) throws EntityNotFoundException;
+
     Meeting getById(long id) throws EntityNotFoundException;
+
     List<Meeting> getAll();
 }

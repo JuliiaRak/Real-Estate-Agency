d5b960cd09f67a9ad2f3f5209ee29615b15d1be0
JuliiaRak <julia.rak.informatyka@gmail.com>
2024-01-30 19:33:05 +0200

    Conflict solvd

---
+++ src/test/java/com/solvd/MeetingServiceTest.java
@@ -0,0 +1,9 @@
+package com.solvd;
+
+import com.solvd.service.*;
+import com.solvd.service.impl.*;
+
+public class MeetingServiceTest {
+    private static final MeetingService MEETING_SERVICE = new MeetingServiceImpl();
+
+}
